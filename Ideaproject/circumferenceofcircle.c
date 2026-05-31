12273ef3026af86d673fe7d9abc088dacee82216
kankush90359-bit <kankush90359@gmail.com>
2026-05-31 12:00:43 +0530

    updated file

---
+++ Ideaproject/circumferenceofcircle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int r;
+    printf("enter the radius of the circle:");
+    scanf("%d",&r);
+    printf("circumference of circle is %f",2*3.14*r);
+    return 0;
+}
