a62f77ad12adc9a3baaeafbc972ef2e9435a44d6
kankush90359-bit <kankush90359@gmail.com>
2026-08-25 23:26:22 +0530

    updated code

--- evenno.cpp
+++ evenno.cpp
@@ -1,1 +1,17 @@
-#include<iostream
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"enter the first number =";
+    cin>>i;
+    cout<<"enter the second number =";
+    cin>>n;
+    while(i<=n){
+        if(i%2==0){
+            cout<<i<<endl;
+        }
+        i++;
+    }
+}
+
